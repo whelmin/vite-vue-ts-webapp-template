308780368a2b9c518bdf3133dfa8d3cbe384a285
whelmin <i@huywen.me>
2023-03-20 15:04:03 +0800

    feat: add app-example multi entrys

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,6 +3,6 @@ module.exports = {
   semi: true,
   trailingComma: 'es5',
   bracketSameLine: true,
-  printWidth: 80,
+  printWidth: 120,
   arrowParens: 'always',
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        appExampleActivity1: fileURLToPath(new URL('./src/app-example/activity1/index.html', import.meta.url)),
+        appExampleActivity2: fileURLToPath(new URL('./src/app-example/activity2/index.html', import.meta.url)),
+      },
+    },
+  },
 });
